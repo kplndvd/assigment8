cceec3eee885d3da331e490b48d37186ae838feb
David Kaplan <kplndvd@gmail.com>
2020-01-04 21:52:41 +0200

    code clean up

--- bank.py
+++ bank.py
@@ -5,65 +5,99 @@ smile = emojis.encode(":smile:")
 fish = emojis.encode(":tropical_fish:")
 clock = emojis.encode(":clock1:")
 bot = emojis.encode(":space_invader:")
+thumbsup = emojis.encode(":thumbsup:")
+sunglasses = emojis.encode(":sunglasses:")
+dancer = emojis.encode(":dancer:")
+astonished = emojis.encode(":astonished:")
+underage = emojis.encode(":underage:")
+unamused = emojis.encode(":unamused:")
+no_good = emojis.encode(":no_good:")
+interrobang = emojis.encode(":interrobang:")
+moneybag = emojis.encode(":moneybag:")
+money_with_wings = emojis.encode(":money_with_wings:")
+dollar = emojis.encode(":dollar:")
+musical_score = emojis.encode(":musical_score:")
+heavy_exclamation_mark = emojis.encode(":heavy_exclamation_mark:")
+ok_hand = emojis.encode(":ok_hand:")
+ok_woman = emojis.encode(":ok_woman:")
+clap = emojis.encode(":clap:")
+checkered_flag = emojis.encode(":checkered_flag:")
+gift = emojis.encode(":gift:")
+sob = emojis.encode(":sob:")
+joy = emojis.encode(":joy:")
+muscle = emojis.encode(":muscle:")
+roller_coaster = emojis.encode("roller_coaster")
+warning = emojis.encode(":warning:")
+surfer = emojis.encode(":surfer:")
+laughing = emojis.encode(":laughing:")
+baby = emojis.encode(":baby:")
+sparkling_heart = emojis.encode(":sparkling_heart:")
+two_hearts = emojis.encode(":two_hearts:")
+heart = emojis.encode(":heart:")
+dog = emojis.encode(":dog:")
+animals = emojis.encode(":dog: :cat: :fish: :mouse: :hamster: :rabbit: :wolf: :frog: :tiger: :koala:"
+                        ":bear: :cow: :boar: :monkey: :horse: :racehorse: :camel: :sheep: :elephant: :panda_face:")
+
 
 bank = {  # responses list:
-    "default_responses": ["Perhaps we can discuss another topic", emojis.encode(" I like that :thumbsup:"),
-                          emojis.encode("Pretty cool :sunglasses:"), emojis.encode(" Dance for me ! :dancer:"),
+    "default_responses": ["Perhaps we can discuss another topic", " I like that " + thumbsup,
+                          "Pretty cool " + sunglasses, " Dance for me ! " + dancer,
                           " Although I'm pretty smart I'm really just a chatbot" + bot, "I'm here to help" + smile],
 
-    "swear_responses": [emojis.encode("You should wash your mouth with some soap! :astonished:"),
-                        emojis.encode("Please use clean language :underage:"),
+    "swear_responses": ["You should wash your mouth with some soap! " + astonished,
+                        "Please use clean language" + underage,
                         "I don't speak Klingon , try typing in English" + three_monkeys,
                         " Looks like you have a typo" + three_monkeys,
-                        emojis.encode("I'm not programmed for dirty language :unamused: "),
-                        emojis.encode("Your mother wouldn't be happy with this :no_good:"),
+                        "I'm not programmed for dirty language " + unamused,
+                        "Your mother wouldn't be happy with this " + no_good,
                         "oopsi you slipped a dirty, lets re-phrase" + three_monkeys,
-                        emojis.encode("I think you want to re-phrase :interrobang: ") ],
+                        "I think you want to re-phrase " + interrobang],
 
-    "financial_responses": [emojis.encode("Show me the money! :moneybag:"), emojis.encode("Get a lotto ticket I've got a good feeling about this :money_with_wings:"),
-                            emojis.encode("It's all about the money :dollar:"),
-                            emojis.encode("If I were a rich man..la la la :musical_score:")],
+    "financial_responses": ["Show me the money! " + moneybag,
+                            "Get a lotto ticket I've got a good feeling about this " + money_with_wings,
+                            "It's all about the money " + dollar,
+                            "If I were a rich man..la la la " + musical_score],
 
-    'no_responses': [emojis.encode("Let's try to be positive about this :dancer:"),
-                     emojis.encode(" No ? How about YES:heavy_exclamation_mark:"), "Think happy thoughts"],
+    'no_responses': ["Let's try to be positive about this " + dancer,
+                     " No ? How about YES"+heavy_exclamation_mark, "Think happy thoughts"],
 
-    "takeoff_responses": [emojis.encode("Ask my partner Google, he will be real useful for that one! :ok_hand:"),
-                          emojis.encode("I'm not sure :ok_woman:"),
+    "takeoff_responses": ["Ask my partner Google, he will be real useful for that one! " + ok_hand,
+                          "I'm not sure " + ok_woman,
                           " figures..", "Why not?", " Pretty much", "You may want to ask some people as well",
                           "Can you try to rephrase?", "I had the same question my self",
-                          "I suggest we change the subject", "mm, well what do you think?"],
+                          "I suggest we change the subject", "mm, well what do you think?",
+                          "That's a great question, you should ask google"],
 
-    "dancing_responses": [emojis.encode("I think that's great! :clap:"), "Wonderful", "I have to agree on that",
+    "dancing_responses": ["I think that's great! " + clap, "Wonderful", "I have to agree on that",
                           "Yep!", "For sure!",
-                          emojis.encode("Go Go Go! :checkered_flag:")],
+                          "Go Go Go! " + checkered_flag],
 
-    "crying_responses": [emojis.encode("Would a tissue help? :gift:"),
-                         emojis.encode("Crying is a good way to let it out :sob:"),
-                         emojis.encode("These tears should turn into tears of joy :sob: :joy:")],
+    "crying_responses": ["Would a tissue help? " + gift,
+                         "Crying is a good way to let it out " + sob,
+                         "These tears should turn into tears of joy " + sob + joy],
 
-    "bored_responses": [" That reminds me of a famous quote ", "What do you think about this quote?", " Famous quote: "],
+    "bored_responses": [" That reminds me of a famous quote ", "What do you think about this quote?",
+                        " Famous quote: "],
 
-    "afraid_responses": ['I\'m just a bot ' + bot, emojis.encode('Have no Fear :surfer:'),
-                         emojis.encode('The entire world is a very narrow bridge :roller_coaster: and the important this is not to be afraid :muscle:'),
-                         emojis.encode('Please do not feed the fears! :warning:')],
+    "afraid_responses": ['I\'m just a bot ' + bot, "Have no Fear " + surfer,
+                         "The entire world is a very narrow bridge " + roller_coaster +
+                         " and the important this is not to be afraid " + muscle,
+                         "Please do not feed the fears! " + warning],
 
-    "funny_responses": [emojis.encode("Ha, listen to this joke :laughing:: "), emojis.encode("Funny :laughing:"),
-                        emojis.encode("I'm feeling ticklish, :baby: reminds me of a joke: "),
-                        emojis.encode("You've got a great sense of humor! :smile:, so do I: ")],
+    "funny_responses": ["Ha, listen to this joke " + laughing, "Funny " + laughing,
+                        "I'm feeling ticklish, " + baby + " reminds me of a joke: ",
+                        "You've got a great sense of humor! " + smile + ", so do I: "],
 
     "waiting_responses": ["Patience is a virtue" + clock, "I've been waiting all my life" + clock],
 
-    "inlove_responses": [emojis.encode("Love Love me Do :sparkling_heart:"),
-                         emojis.encode("Love is in the air :two_hearts:"), emojis.encode(":heart:")],
+    "inlove_responses": ["Love Love me Do " + sparkling_heart,
+                         "Love is in the air " + two_hearts, heart],
 
     "heartbroke_responses": ["There are many fish in the sea" + fish, "Don't worry be happy" + smile,
-                             emojis.encode(":broken_heart:"),
-                             "Something good will happen soon, you'll see" + smile],
+                             "broken_heart", "Something good will happen soon, you'll see" + smile],
 
-    "dog_responses": [" I love pets! I have a dog in my self as you can see", emojis.encode(" I'm a dog type :dog:"),
-                      emojis.encode(":dog:"), emojis.encode(
-            " I like many animals :dog: :cat: :fish: :mouse: :hamster: :rabbit: :wolf: :frog: :tiger: :koala:"
-            ":bear: :cow: :boar: :monkey: :horse: :racehorse: :camel: :sheep: :elephant: :panda_face: ")],
+    "dog_responses": [" I love pets! I have a dog in my self as you can see", " I'm a dog type " + dog,
+                      dog, " I like many animals " + animals],
 
     # trigger list:
     'time_list': ["time", "date", "zone", "hour", "minute", "hours", "minutes", "seconds", "hr", "min", "sec"],
@@ -88,3 +122,4 @@ bank = {  # responses list:
     'heartbroke_words': ["heart", "heart broken", "broke my heart"],
     'dog_words': ["pets", "pet", "dog", "animals", "cat", "cats", "parrot", "parrots", "hamster"],
 }
+
